3d71c338ac69c6446a79dabab3403d68999eef00
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2023-04-24 10:15:26 -0400

    sgsdgdsgsdgsdgshfsh

--- nyc-project/src/router/index.js
+++ nyc-project/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
-const monkeyworld = 'https://data.cityofnewyork.us/resource/jb7j-dtam.json'
+const link = 'https://data.cityofnewyork.us/resource/jb7j-dtam.json'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
