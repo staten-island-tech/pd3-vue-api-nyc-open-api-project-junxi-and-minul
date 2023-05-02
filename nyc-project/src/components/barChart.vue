54a78aa39d8e71af9be40c589e776d0bf00d536a
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2023-05-02 09:38:24 -0400

    dzfdzfzdfdzfdzfzfdzf

---
+++ nyc-project/src/components/barChart.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Bar :data="chartData" :options="chartOptions" />
+  </div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+
+ChartJS.register(Tooltip, Legend, ArcElement)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  props: {
+    chartData: {
+      type: Object,
+      required: true
+    },
+    chartOptions: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      retrievedData: false
+    }
+  }
+}
+</script>
--- nyc-project/src/views/lineView.vue
+++ nyc-project/src/views/lineView.vue
@@ -1,10 +1,52 @@
 <script>
+import BarChart from '../components/barChart.vue'
 export default {
-  name: `barChart`,
-  components: {}
+  mounted() {
+    this.BarCharts()
+  },
+  name: 'barChart',
+  components: { BarChart },
+  data() {
+    return {
+      chartData: {
+        labels: ["Alzheimer's Disease", 'Viral Hepatitis', 'Septicemia'],
+        datasets: [{ data: [] }]
+      },
+      chartOptions: {
+        responsive: true,
+        backgroundColor: ['#0000FF', '#FF0000', '#00FF00']
+      },
+      retrievedData: false
+    }
+  },
+  methods: {
+    BarCharts: async function getCauses() {
+      let res = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
+      let data = await res.json()
+      console.log(data)
+      const array = data.filter((person) => person.leading_cause === "Alzheimer's Disease (G30)")
+      const array2 = data.filter((person) => person.leading_cause === 'Viral Hepatitis (B15-B19)')
+      const array3 = data.filter((person) => person.leading_cause === 'Septicemia (A40-A41)')
+      this.chartData.datasets[0].data.push(array.length)
+      this.retrievedData = true
+      this.chartData.datasets[0].data.push(array2.length)
+      this.retrievedData = true
+      this.chartData.datasets[0].data.push(array3.length)
+      this.retrievedData = true
+    }
+  }
 }
 </script>
 
-<template></template>
+<template>
+  <div>
+    <BarChart
+      class="chart"
+      v-if="retrievedData"
+      :chartData="chartData"
+      :chartOptions="chartOptions"
+    />
+  </div>
+</template>
 
 <style></style>
